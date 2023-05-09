1306697fc168a6efd40fdcfff7a0966575ffb550
Mayank Mahor <100315888+themayankmahor@users.noreply.github.com>
2023-05-10 02:53:07 +0530

    10/05/2023

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Home = () =>
+{
+    return(
+        <div className="container">
+
+            <div className="text-center">
+            <h1>This is Home page</h1>
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default Home;
